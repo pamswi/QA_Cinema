78414c62e51352785fc864eda98a5877b03c44fd
Alexander Bonfield <bonfield.alex@gmail.com>
2023-09-10 22:17:34 +0100

    AB

--- application/routes.py
+++ application/routes.py
@@ -72,9 +72,10 @@ def api_view_screenings(movie_id):
     return jsonify(screening_data)
 
 
-@app.route("/listings")
+@app.route("/listings", methods=["GET"])
 def listings():
-    return render_template("gallery.html")
+    all_films = Movie.query.all()
+    return render_template("listings.html", films=all_films)
 
 @app.route('/newreleases', methods=['GET'])
 def new_releases(): 
