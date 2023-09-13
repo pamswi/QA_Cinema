11e1162a81627882d1859868a7d4a318a1378c21
Pam Swi <pamswiderska@gmail.com>
2023-09-13 14:08:30 +0100

    PS: test app update

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,8 +8,8 @@ from werkzeug.security import generate_password_hash
 
 class TestBase(TestCase):
     def create_app(self):
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cinema.db"
         app.config.update(
-            SQLALCHEMY_DATABASE_URI = "sqlite:///cinema.db",
             # SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI"),
             #export DATABASE_URI=mysql+pymysql://root:pass@localhost/test_qa_cinema
             DEBUG=True,
