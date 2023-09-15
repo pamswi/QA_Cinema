59a5e2f6033cbb35e106fe7497ce9dc05dcdc0cb
Kes Germany <kesgermany@yahoo.com>
2023-09-15 10:44:44 +0100

    EG: fixing port and db queries

--- application/__init__.py
+++ application/__init__.py
@@ -5,8 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 db = SQLAlchemy()
 
 app.config["SESSION_PERMANENT"] = False
